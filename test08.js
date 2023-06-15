dfe61045c689d93a39f22811c455f8c9a4466909
Jamie01 <seventeen0224@gmail.com>
2023-06-15 21:43:17 +0900

    write, read, unlink, etc

--- test08.js
+++ test08.js
@@ -10,6 +10,29 @@ const fileOptions = async()=>{
         const data = await fs.readFile('./datas/lorem.txt', "utf-8")
         console.log("1. readFile: ", data);
         console.log("2. console.log ");
+
+        await fsPromises.writeFile("./datas/newWrite.txt", data);
+        console.log('3. wirte complete');
+
+        await fsPromises.appendFile("./datas/newWrite.txt", "append txt");
+        console.log('4. appendFile');
+
+        await fsPromises.rename("./datas/newWrite.txt", "./datas/rename.txt", err=>{if(err)throw err});
+        console.log('5. rename');
+
+        if(!fsPromises.existsSync("./model")){
+            await fsPromises.mkdir('./model', err=>{if(err)throw err});
+        }
+
+        console.log('6. mkdir')
+        // await fs.readdir('./', (err, filelist)=>{
+        //     console.log(filelist);
+        // })
+        const filelist = await fsPromises.readdir('./',err=>{if(err)throw err});
+        console.log("7. ", filelist);
+
+        await fsPromises.unlink(path.join(__dirname, "datas", "rename.txt"));
+        console.log("8. unlink");
     }catch(err){
         console.log(err);
     }finally{
